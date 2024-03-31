940824caff1d8fc09d2a55b47817ee177264a19e
dominikdeimel <info@dominikdeimel.com>
2024-03-31 20:19:21 +0200

    chore(layout): refactored navigation constants

---
+++ libs/constants/src/lib/navigation.constants.ts
@@ -0,0 +1,9 @@
+import { Link } from '@ng-tool-collection/models';
+
+export const NAVIGATION: Link[] = [{
+  label: 'Passwort Generator',
+  path: 'password-generator'
+}, {
+  label: 'Url Rewrites',
+  path: 'url-rewrites'
+}];
--- libs/ui/src/lib/components/drawer/drawer.component.ts
+++ libs/ui/src/lib/components/drawer/drawer.component.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Link } from '@ng-tool-collection/models';
+import { NAVIGATION } from '@ng-tool-collection/constants';
 
 @Component({
   selector: 'lib-drawer',
   templateUrl: './drawer.component.html'
 })
-export class DrawerComponent {
-  navItems: Link[] = [{
-    label: 'Passwort Generator',
-    path: 'password-generator'
-  }, {
-    label: 'Url Rewrites',
-    path: 'url-rewrites'
-  }];
+export class DrawerComponent implements OnInit {
+  navItems: Link[] = [];
+
+  ngOnInit (): void {
+    this.navItems = NAVIGATION;
+  }
 }
