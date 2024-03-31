cac0cb6f14d996847a672240f10c7430ebd2e085
dominikdeimel <info@dominikdeimel.com>
2024-03-31 20:19:21 +0200

    chore(layout): refactored navigation constants

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { DrawerComponent } from './components/drawer/drawer.component';
 
 @NgModule({
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   declarations: [NavbarComponent, DrawerComponent],
   exports: [NavbarComponent, DrawerComponent]
 })
