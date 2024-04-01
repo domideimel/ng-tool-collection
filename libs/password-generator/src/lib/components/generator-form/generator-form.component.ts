4d7592b69498d26db2b4447af907104ff53b59e8
dominikdeimel <info@dominikdeimel.com>
2024-04-01 17:59:19 +0200

    chore(pw gen): refactored logic into a service

--- libs/password-generator/src/lib/components/generator-form/generator-form.component.ts
+++ libs/password-generator/src/lib/components/generator-form/generator-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
-import { FormModel, GenerationProperties, RandomFunc } from '@ng-tool-collection/models';
+import { FormModel, GenerationProperties } from '@ng-tool-collection/models';
 import { Validators } from '@angular/forms';
-import { isEmpty, omitBy, random, range, sample } from 'lodash-es';
+import { PasswordGeneratorService } from '../../services/password-generator.service';
 
 @Component({
   selector: 'lib-generator-form',
@@ -40,39 +40,14 @@ export class GeneratorFormComponent {
   };
   password = signal<string>('');
 
+  constructor (private pgenService: PasswordGeneratorService) {}
+
   onSubmit (value: GenerationProperties) {
-    this.password.set(this.generatePassword(value));
+    this.password.set(this.pgenService.generatePassword(value));
   }
 
   async copyToClipboard () {
     await navigator.clipboard.writeText(this.password());
   }
-
-  private createSymbolString (): string[] {
-    const asciiCharsRanges: number[] = [...range(33, 48), ...range(58, 65), ...range(91, 97), ...range(123, 127)];
-    return asciiCharsRanges.map(char => String.fromCharCode(char));
-  }
-
-  private getRandomChar = (start: number, end: number): string => String.fromCharCode(random(start, end));
-
-  private getRandomSymbol = (symbols: string[] = this.createSymbolString()): string => symbols[random(0, symbols.length - 1)];
-
-  private randomFunc: RandomFunc = {
-    lower: () => this.getRandomChar(97, 122),
-    upper: () => this.getRandomChar(65, 90),
-    number: () => this.getRandomChar(48, 57),
-    symbol: this.getRandomSymbol
-  };
-
-  private generatePassword = ({ length, ...args }: GenerationProperties): string => {
-    const filteredTypes = omitBy(args, value => !value);
-    if (isEmpty(filteredTypes)) return '';
-    const typesArray = Object.entries(filteredTypes);
-
-    return Array.from({ length }, () => {
-      const [type] = sample(typesArray) as [keyof typeof filteredTypes, boolean];
-      return this.randomFunc[type as keyof RandomFunc]();
-    }).join('');
-  };
 
 }
