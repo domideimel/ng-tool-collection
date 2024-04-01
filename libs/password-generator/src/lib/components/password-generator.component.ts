be9c8db8f3871b9ae9f74903d445f37a4666a157
dominikdeimel <info@dominikdeimel.com>
2024-04-01 15:44:15 +0200

    chore(form): fixed form

--- libs/password-generator/src/lib/components/password-generator.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-password-generator',
-  templateUrl: './password-generator.component.html'
-})
-export class PasswordGeneratorComponent {}
--- libs/password-generator/src/lib/password-generator.module.ts
+++ libs/password-generator/src/lib/password-generator.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { passwordGeneratorRoutes } from './lib.routes';
-import { PasswordGeneratorComponent } from './components/password-generator.component';
+import { GeneratorFormComponent } from './components/generator-form/generator-form.component';
+import { UiModule } from '@ng-tool-collection/ui';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(passwordGeneratorRoutes)],
-  declarations: [PasswordGeneratorComponent]
+  imports: [CommonModule, RouterModule.forChild(passwordGeneratorRoutes), UiModule],
+  declarations: [GeneratorFormComponent]
 })
 export class PasswordGeneratorModule {}
