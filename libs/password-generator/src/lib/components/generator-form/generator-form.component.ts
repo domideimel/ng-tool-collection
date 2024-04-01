4c9e2d10da7b6430d535611e42b134108bcb5665
dominikdeimel <info@dominikdeimel.com>
2024-04-01 17:50:41 +0200

    chore(pw gen): added functionality

--- libs/password-generator/src/lib/components/generator-form/generator-form.component.ts
+++ libs/password-generator/src/lib/components/generator-form/generator-form.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { FormModel } from '@ng-tool-collection/models';
+import { Component, signal } from '@angular/core';
+import { FormModel, GenerationProperties, RandomFunc } from '@ng-tool-collection/models';
+import { Validators } from '@angular/forms';
+import { isEmpty, omitBy, random, range, sample } from 'lodash-es';
 
 @Component({
   selector: 'lib-generator-form',
@@ -8,31 +10,69 @@ import { FormModel } from '@ng-tool-collection/models';
 export class GeneratorFormComponent {
   formModel: FormModel = {
     items: [{
-      label: 'Länge',
+      label: 'Passwort Länge',
       controlName: 'length',
-      type: 'range'
+      type: 'range',
+      value: 6,
+      validators: [Validators.required, Validators.min(6), Validators.max(100)]
     }, {
       label: 'Großbuchstaben verwenden',
-      controlName: 'uppercase',
-      type: 'checkbox'
+      controlName: 'upper',
+      type: 'checkbox',
+      value: true
     }, {
       label: 'Kleinbuchstaben verwenden',
-      controlName: 'lowercase',
-      type: 'checkbox'
+      controlName: 'lower',
+      type: 'checkbox',
+      value: true
     }, {
       label: 'Sonderzeichen verwenden',
-      controlName: 'specialCharacters',
-      type: 'checkbox'
+      controlName: 'symbol',
+      type: 'checkbox',
+      value: true
     }, {
       label: 'Zahlen verwenden',
-      controlName: 'numbers',
-      type: 'checkbox'
+      controlName: 'number',
+      type: 'checkbox',
+      value: true
     }],
     submitButtonLabel: 'Passwort generieren'
   };
+  password = signal<string>('');
 
-  onSubmit (value: any) {
-    console.log(value);
+  onSubmit (value: GenerationProperties) {
+    this.password.set(this.generatePassword(value));
   }
+
+  async copyToClipboard () {
+    await navigator.clipboard.writeText(this.password());
+  }
+
+  private createSymbolString (): string[] {
+    const asciiCharsRanges: number[] = [...range(33, 48), ...range(58, 65), ...range(91, 97), ...range(123, 127)];
+    return asciiCharsRanges.map(char => String.fromCharCode(char));
+  }
+
+  private getRandomChar = (start: number, end: number): string => String.fromCharCode(random(start, end));
+
+  private getRandomSymbol = (symbols: string[] = this.createSymbolString()): string => symbols[random(0, symbols.length - 1)];
+
+  private randomFunc: RandomFunc = {
+    lower: () => this.getRandomChar(97, 122),
+    upper: () => this.getRandomChar(65, 90),
+    number: () => this.getRandomChar(48, 57),
+    symbol: this.getRandomSymbol
+  };
+
+  private generatePassword = ({ length, ...args }: GenerationProperties): string => {
+    const filteredTypes = omitBy(args, value => !value);
+    if (isEmpty(filteredTypes)) return '';
+    const typesArray = Object.entries(filteredTypes);
+
+    return Array.from({ length }, () => {
+      const [type] = sample(typesArray) as [keyof typeof filteredTypes, boolean];
+      return this.randomFunc[type as keyof RandomFunc]();
+    }).join('');
+  };
 
 }
