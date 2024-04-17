79cbcd48297da97aed4855274cbb64f047ff0e5e
dominikdeimel <info@dominikdeimel.com>
2024-04-17 21:26:25 +0200

    chore(pw gen): changed error supression

--- libs/ui/src/lib/components/form/validators/atLeastOneChecked.validator.ts
+++ libs/ui/src/lib/components/form/validators/atLeastOneChecked.validator.ts
@@ -1,7 +1,8 @@
 import { FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export const atLeastOneCheckedValidator = (controlNames: string[]): ValidatorFn => {
-  // @ts-ignore
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
   return (group: FormGroup): ValidationErrors | null => {
     const hasAtLeastOneChecked = controlNames.some(controlName => group.controls[controlName].value);
 
