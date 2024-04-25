bb10f1bea322ffe92f3fa3069d7952d8a70d789a
dominikdeimel <info@dominikdeimel.com>
2024-04-25 20:26:09 +0200

    chore(pw gen): added toast component

---
+++ libs/models/src/lib/toasts.model.ts
@@ -0,0 +1,7 @@
+export interface Toast {
+  alertType: 'alert-info' | 'alert-success' | 'alert-warning' | 'alert-error';
+  message: string;
+}
+
+export type HorizontalAlignment = 'toast-start' | 'toast-center' | 'toast-end'
+export type VerticalAlignment = 'toast-top' | 'toast-middle' | 'toast-bottom'
