a72ec65eeb4e2109db767c8d561e4d1bd243b9d8
dominikdeimel <info@dominikdeimel.com>
2024-04-28 11:01:24 +0200

    chore(url-rewrites): base URL-Rewrite

--- libs/url-rewrites/src/lib/components/url-rewrites.component.ts
+++ libs/url-rewrites/src/lib/components/url-rewrites.component.ts
@@ -1,8 +1,32 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormModel } from '@ng-tool-collection/models';
+import { Validators } from '@angular/forms';
+import { UrlRewriteModel } from '../../../../models/src/lib/urlRewrite.model';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'lib-url-rewrites',
   templateUrl: './url-rewrites.component.html'
 })
-export class UrlRewritesComponent {}
+export class UrlRewritesComponent {
+  formModel: FormModel = {
+    submitButtonLabel: 'Generieren',
+    rows: [[{
+      label: 'Alte URL',
+      controlName: 'oldUrl_1',
+      type: 'url',
+      validators: [Validators.min(1)],
+      placeholder: 'https://oldUrl.com'
+    }, {
+      label: 'Neue URL',
+      controlName: 'newUrl_1',
+      type: 'url',
+      validators: [Validators.min(1)],
+      placeholder: 'https://newUrl.com'
+    }]]
+  };
+
+  onSubmit (value: UrlRewriteModel[]) {
+    console.log(value);
+  }
+}
