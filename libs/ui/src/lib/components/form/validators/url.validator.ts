506771fe55801592dac18166514280c3bf5dee7e
dominikdeimel <info@dominikdeimel.com>
2024-04-28 15:56:04 +0200

    chore(general): optimized bundle output

--- libs/ui/src/lib/components/form/validators/url.validator.ts
+++ libs/ui/src/lib/components/form/validators/url.validator.ts
@@ -1,5 +1,5 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
-import { isURL } from 'validator';
+import isURL from 'validator/es/lib/isURL';
 
 export function urlValidator (control: AbstractControl): ValidationErrors | null {
   if (control.value && !isURL(control.value)) {
