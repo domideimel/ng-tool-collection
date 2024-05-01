b1fe140257f6045badd570f4d72ee24929cecdbc
dominikdeimel <info@dominikdeimel.com>
2024-05-01 11:28:07 +0200

    chore(general): added favicons and meta tags updates

--- libs/home/src/lib/components/card-grid/card-grid.component.ts
+++ libs/home/src/lib/components/card-grid/card-grid.component.ts
@@ -1,12 +1,22 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 import { Link } from '@ng-tool-collection/models';
 import { NAVIGATION } from '@ng-tool-collection/constants';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'lib-card-grid',
   templateUrl: './card-grid.component.html'
 })
-export class CardGridComponent {
+export class CardGridComponent implements OnInit {
   cards = signal<Link[]>(NAVIGATION);
+
+  constructor (private meta: Meta) { }
+
+  ngOnInit () {
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Entdecke unsere umfangreiche Sammlung an Tools, die entwickelt wurden, um deinen Alltag zu erleichtern. Von Produktivitätssteigerung bis hin zur Organisation – finde genau das, was du brauchst, um effizienter zu arbeiten und mehr zu erreichen.'
+    });
+  }
 }
--- libs/password-generator/src/lib/components/generator-wrapper/generator-wrapper.component.ts
+++ libs/password-generator/src/lib/components/generator-wrapper/generator-wrapper.component.ts
@@ -1,8 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'lib-generator-wrapper',
   templateUrl: './generator-wrapper.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GeneratorWrapperComponent {}
+export class GeneratorWrapperComponent implements OnInit {
+  constructor (private meta: Meta) {}
+
+  ngOnInit () {
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Erstelle hier benutzerdefinierte Passwörter jeder beliebigen Länge - sicher und bequem!'
+    });
+  }
+}
