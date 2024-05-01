fa3e55c87a2a5269327dc769f4f9a3f896f05b28
dominikdeimel <info@dominikdeimel.com>
2024-05-01 21:13:16 +0200

    chore(service-worker): added service-worker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { NgxWebstorageModule } from 'ngx-webstorage';
 import { provideHotToastConfig } from '@ngneat/hot-toast';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     importProvidersFrom(NgxWebstorageModule.forRoot()),
-    provideHotToastConfig()
+    provideHotToastConfig(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
