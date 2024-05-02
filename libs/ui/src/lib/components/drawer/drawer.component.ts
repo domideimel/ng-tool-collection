9b2baba011c06cac348838c8e81a2f0440123a2e
dominikdeimel <info@dominikdeimel.com>
2024-05-02 17:10:17 +0200

    Add toggle functionality to drawer component
    Added a toggle function to the drawer component in the UI library. This function will allow the drawer to open and close based on user interaction. Additionally, the drawer now closes automatically when a navigation event completes. Implemented necessary lifecycle hooks and event subscriptions to manage this behavior.

--- libs/ui/src/lib/components/drawer/drawer.component.ts
+++ libs/ui/src/lib/components/drawer/drawer.component.ts
@@ -1,16 +1,45 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { Link } from '@ng-tool-collection/models';
-import { NAVIGATION } from '@ng-tool-collection/constants';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+  signal,
+} from "@angular/core";
+import { Link } from "@ng-tool-collection/models";
+import { NAVIGATION } from "@ng-tool-collection/constants";
+import { BehaviorSubject, filter, Subscription } from "rxjs";
+import { NavigationEnd, Router } from "@angular/router";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  selector: 'lib-drawer',
-  templateUrl: './drawer.component.html'
+  selector: "lib-drawer",
+  templateUrl: "./drawer.component.html",
 })
-export class DrawerComponent {
+export class DrawerComponent implements OnInit, OnDestroy {
   navItems = signal<Link[]>(NAVIGATION);
+  isOpen: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  routerSubscription!: Subscription;
 
-  get currentYear () {
+  constructor(private router: Router) {}
+
+  get currentYear() {
     return new Date().getFullYear();
+  }
+
+  ngOnInit() {
+    this.routerSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.isOpen.next(false);
+      });
+  }
+
+  ngOnDestroy() {
+    this.routerSubscription.unsubscribe();
+  }
+
+  toggleOpen(event: EventTarget | null): void {
+    if (!event) return;
+    this.isOpen.next((event as HTMLInputElement).checked);
   }
 }
