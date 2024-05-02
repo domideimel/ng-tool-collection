ccc0e9303b64433f4308ae9464e1097bb8792d27
dominikdeimel <info@dominikdeimel.com>
2024-05-02 16:58:43 +0200

    Add theme switcher component and service
    This update adds a new service (theme.service.ts) to manage theme status and a new theme switcher component to the UI library. The theme switcher allows users to switch between dark and light modes. This toggle has been added to the navbar component.

---
+++ libs/ui/src/lib/components/theme-switcher/theme-switcher.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ThemeService } from "./services/theme.service";
+import { toSignal } from "@angular/core/rxjs-interop";
+
+@Component({
+  selector: "lib-theme-switcher",
+  templateUrl: "./theme-switcher.component.html",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ThemeSwitcherComponent {
+  isDarkTheme = toSignal(this.themeService.isDarkTheme);
+
+  constructor(public themeService: ThemeService) {}
+
+  switchTheme(event: EventTarget | null): void {
+    if (!event) return;
+    this.themeService.setDarkTheme((event as HTMLInputElement).checked);
+  }
+}
