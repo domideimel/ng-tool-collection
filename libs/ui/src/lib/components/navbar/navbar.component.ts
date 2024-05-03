7ae5919b042d64c08b8be301c5075e5b7c58294a
dominikdeimel <info@dominikdeimel.com>
2024-05-03 22:17:17 +0200

    Update drawer and navbar components with open change functionality
    The commit includes code changes that introduce navbar open state change functionality. The drawer component now emits a signal when its open state changes and the navbar closes automatically when clicked outside. Furthermore, adjustments in the GeneratorFormComponent have been made to enhance password generation.

--- libs/ui/src/lib/components/navbar/navbar.component.ts
+++ libs/ui/src/lib/components/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { Link } from '@ng-tool-collection/models';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  model,
+} from "@angular/core";
+import { Link } from "@ng-tool-collection/models";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  selector: 'lib-navbar',
-  templateUrl: './navbar.component.html'
+  selector: "lib-navbar",
+  templateUrl: "./navbar.component.html",
 })
 export class NavbarComponent {
-  title = input('Tool Collection');
+  title = input("Tool Collection");
   links = input.required<Link[]>();
+  openNavbar = model<boolean | null>();
+
+  onOpenClick() {
+    if (this.openNavbar()) {
+      this.openNavbar.set(false);
+      return;
+    }
+    this.openNavbar.set(true);
+  }
 }
