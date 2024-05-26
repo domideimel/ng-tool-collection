cc4eaae0f2af087bddf2d2377e4b011526bcf480
dominikdeimel <info@dominikdeimel.com>
2024-05-26 17:59:24 +0200

    Implement zoneless change detection and remove zone.js polyfills
    Zoneless change detection was added to the application configuration, and zone.js was removed from the project's polyfills. This means that the app will no longer automatically trigger change detection for async operations, potentially improving performance.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
-import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
-import { NgxWebstorageModule } from 'ngx-webstorage';
-import { provideHotToastConfig } from '@ngneat/hot-toast';
-import { provideServiceWorker } from '@angular/service-worker';
+import { ApplicationConfig, importProvidersFrom, isDevMode, provideExperimentalZonelessChangeDetection } from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { appRoutes } from './app.routes'
+import { NgxWebstorageModule } from 'ngx-webstorage'
+import { provideHotToastConfig } from '@ngneat/hot-toast'
+import { provideServiceWorker } from '@angular/service-worker'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    provideExperimentalZonelessChangeDetection()
   ]
-};
+}
