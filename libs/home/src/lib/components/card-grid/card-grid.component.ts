d8f45767e9e7dd18fe819ec1ac56fc28a6c935d5
dominikdeimel <info@dominikdeimel.com>
2024-07-24 16:22:18 +0200

    Refactor card components to use Angular Material
    Updated the CardComponent and CardGridComponent to utilize Angular Material components such as MatCardModule and MatGridList for better styling and consistency. Modified HTML templates accordingly to reflect these changes.

--- libs/home/src/lib/components/card-grid/card-grid.component.ts
+++ libs/home/src/lib/components/card-grid/card-grid.component.ts
@@ -1,25 +1,27 @@
-import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core'
-import { Link } from '@ng-tool-collection/models'
-import { NAVIGATION } from '@ng-tool-collection/constants'
-import { Meta } from '@angular/platform-browser'
-import { CardComponent } from '@ng-tool-collection/ui'
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { Link } from '@ng-tool-collection/models';
+import { NAVIGATION } from '@ng-tool-collection/constants';
+import { Meta } from '@angular/platform-browser';
+import { CardComponent } from '@ng-tool-collection/ui';
+import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'lib-card-grid',
   templateUrl: './card-grid.component.html',
   standalone: true,
-  imports: [CardComponent]
+  imports: [CardComponent, MatGridList, MatGridTile],
 })
 export class CardGridComponent implements OnInit {
-  cards = signal<Link[]>(NAVIGATION)
+  cards = signal<Link[]>(NAVIGATION);
 
-  constructor (private meta: Meta) { }
+  constructor(private meta: Meta) {}
 
-  ngOnInit () {
+  ngOnInit() {
     this.meta.updateTag({
       name: 'description',
-      content: 'Entdecke unsere umfangreiche Sammlung an Tools, die entwickelt wurden, um deinen Alltag zu erleichtern. Von Produktivitätssteigerung bis hin zur Organisation – finde genau das, was du brauchst, um effizienter zu arbeiten und mehr zu erreichen.'
-    })
+      content:
+        'Entdecke unsere umfangreiche Sammlung an Tools, die entwickelt wurden, um deinen Alltag zu erleichtern. Von Produktivitätssteigerung bis hin zur Organisation – finde genau das, was du brauchst, um effizienter zu arbeiten und mehr zu erreichen.',
+    });
   }
 }
--- libs/ui/src/lib/components/card/card.component.ts
+++ libs/ui/src/lib/components/card/card.component.ts
@@ -1,17 +1,19 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core'
-import { RouterLink } from '@angular/router'
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatAnchor } from '@angular/material/button';
 
 @Component({
   selector: 'lib-card',
   templateUrl: './card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [RouterLink]
+  imports: [RouterLink, MatCardModule, MatAnchor],
 })
 export class CardComponent {
-  title = input.required<string>()
-  description = input('')
-  link = input('')
-  linkText = input('Zum Tool')
-  showLink = input(true)
+  title = input.required<string>();
+  description = input('');
+  link = input('');
+  linkText = input('Zum Tool');
+  showLink = input(true);
 }
