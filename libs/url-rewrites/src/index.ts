90cda1ac43426e772d09e0e3d06cc9af51cca3f2
dominikdeimel <info@dominikdeimel.com>
2024-09-28 17:51:32 +0200

    Remove redundant modules and update component imports
    Deleted unused Angular modules to reduce code clutter. Updated related component imports and route configurations to maintain functionality.

--- libs/url-rewrites/src/index.ts
+++ libs/url-rewrites/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib/url-rewrites.module'
-export * from './lib/lib.routes'
-export * from './lib/components/url-rewrites.component'
-export * from './lib/services/url-rewrites.service'
+export * from './lib/lib.routes';
+export * from './lib/components/url-rewrites.component';
+export * from './lib/services/url-rewrites.service';
