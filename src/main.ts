e77eff0068a009877886734aafc8b29046a0b6bb
dominikdeimel <info@dominikdeimel.com>
2024-12-03 20:35:08 +0100

    Use try-catch for bootstrapApplication async call
    This commit updates the main.ts file to utilize a try-catch block for handling errors during the asynchronous bootstrapApplication function call, replacing the previous .catch() method. This change improves error handling and readability of the code. Additionally, single quotes have been standardized for import statements.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import { appConfig } from "./app/app.config";
-import { AppComponent } from "./app/app.component";
-import "@total-typescript/ts-reset";
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import '@total-typescript/ts-reset';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+try {
+  await bootstrapApplication(AppComponent, appConfig);
+} catch (e) {
+  console.error(e);
+}
