7333dc7e20566ef27e73825a7b6ac128e4abb813
dominikdeimel <info@dominikdeimel.com>
2024-12-18 12:07:29 +0100

    Refine clipboard utility error handling.
    Enhanced clipboard utility to properly handle missing APIs and invalid data. Returned types have been adjusted for consistency, improving overall robustness.

--- libs/password-generator/src/lib/utils/copy-to-clipboard.utils.ts
+++ libs/password-generator/src/lib/utils/copy-to-clipboard.utils.ts
@@ -1,10 +1,10 @@
-import { catchError, map, of, switchMap, throwError } from 'rxjs';
+import { catchError, map, Observable, of, switchMap, throwError } from 'rxjs';
 import { fromPromise } from 'rxjs/internal/observable/innerFrom';
 import { State } from '@ng-tool-collection/models';
 
 export const copyToClipboard = <T>(data: T) => {
-  if (!navigator.clipboard || !navigator.clipboard.writeText) {
-    return of(State.ERROR);
+  if (!navigator?.clipboard || !navigator?.clipboard?.writeText || !data) {
+    return throwError(() => State.ERROR) as Observable<State>;
   }
 
   const dataToCopy$ = of(data).pipe(map(data => (typeof data === 'string' ? data : JSON.stringify(data))));
@@ -13,7 +13,7 @@ export const copyToClipboard = <T>(data: T) => {
     switchMap(dataToCopy => fromPromise(navigator.clipboard.writeText(dataToCopy))),
     catchError(err => {
       console.error(err);
-      return throwError(() => State.ERROR);
+      return throwError(() => State.ERROR) as Observable<State>;
     }),
     map(() => State.SUCCESS),
   );
