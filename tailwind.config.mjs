646f7d9386f983ad972da8a50a5d40ffb3ffe90d
dominikdeimel <info@dominikdeimel.com>
2024-12-20 12:06:47 +0100

    Add custom DaisyUI theme and disable default container
    Introduce a custom theme, "domi," with defined colors using DaisyUI. Disable Tailwind's default container plugin and replace it with a custom container setup for better customization.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,42 @@
 import { createGlobPatternsForDependencies } from '@nx/angular/tailwind';
+
+import daisyui from 'daisyui';
 import { join } from 'path';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'), ...createGlobPatternsForDependencies(__dirname)],
   daisyui: {
-    themes: ['black', 'cupcake'],
-  },
-  theme: {
-    extend: {},
-    container: {
-      center: true,
-      padding: '1rem',
-      screens: {
-        '2xl': '1400px',
+    themes: [
+      'black',
+      'cupcake',
+      {
+        domi: {
+          primary: '#00bbff',
+          secondary: '#00d5f1',
+          accent: '#ff5500',
+          neutral: '#181211',
+          'base-100': '#2e2726',
+          info: '#0082ff',
+          success: '#00c956',
+          warning: '#ff8200',
+          error: '#ff005a',
+        },
       },
-    },
+    ],
   },
-  plugins: [require('daisyui')],
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    daisyui,
+    ({ addComponents, theme }) => {
+      addComponents({
+        '.container': {
+          '@apply px-4 mx-auto': {},
+          maxWidth: theme('screens.2xl'),
+        },
+      });
+    },
+  ],
 };
