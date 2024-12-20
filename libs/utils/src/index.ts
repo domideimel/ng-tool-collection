a3a62a03476ce9d7c2a483ad2039b08843570440
dominikdeimel <info@dominikdeimel.com>
2024-12-20 12:05:59 +0100

    Add new directives: clickOutside and observeVisibility
    Introduced ClickOutsideDirective to handle clicks outside an element and ObserveVisibilityDirective to track element visibility using IntersectionObserver. Both directives are standalone and enhance usability in the utilities library.

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/utils/lodash.utils';
 export * from './lib/utils/copy-to-clipboard.utils';
 export * from './lib/directives/copy-to-clipboard.directive';
+export * from './lib/directives/click-outside.directive';
+export * from './lib/directives/observe-visibility.directive';
---
+++ libs/utils/src/lib/directives/observe-visibility.directive.ts
@@ -0,0 +1,60 @@
+import { computed, Directive, effect, ElementRef, inject, input, OnDestroy, output } from '@angular/core';
+import { delay, filter, Subject } from 'rxjs';
+
+@Directive({
+  // eslint-disable-next-line @angular-eslint/directive-selector
+  selector: '[observeVisibility]',
+  standalone: true,
+})
+export class ObserveVisibilityDirective implements OnDestroy {
+  debounceTime = input(0);
+  threshold = input(0.1);
+
+  visible = output<HTMLElement>();
+
+  private subject$ = new Subject<{
+    entry: IntersectionObserverEntry;
+    observer: IntersectionObserver;
+  }>();
+  observer = computed<IntersectionObserver>(() => {
+    const options = {
+      rootMargin: '0px',
+      threshold: this.threshold(),
+    };
+
+    const isIntersecting = (entry: IntersectionObserverEntry) => {
+      return entry.isIntersecting || entry.intersectionRatio > 0;
+    };
+
+    return new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (isIntersecting(entry)) {
+          this.subject$.next({ entry, observer });
+        }
+      });
+    }, options);
+  });
+  private element = inject(ElementRef);
+
+  constructor() {
+    effect(() => {
+      this.observer().observe(this.element.nativeElement);
+
+      this.subject$.pipe(delay(this.debounceTime()), filter(Boolean)).subscribe(async ({ entry, observer }) => {
+        const target = entry.target as HTMLElement;
+
+        if (entry.isIntersecting) {
+          this.visible.emit(target);
+        }
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.observer()) {
+      this.observer().disconnect();
+    }
+
+    this.subject$.complete();
+  }
+}
