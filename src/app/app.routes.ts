8e715898a08b0025fd271e12433d4cdab1c929cf
dominikdeimel <info@dominikdeimel.com>
2025-02-24 20:07:16 +0100

    Refactor routes to remove direct home component loading.
    The home component loading logic was removed in favor of a cleaner lazy-loaded tools module setup. This simplifies route definitions and improves maintainability.
    
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,6 @@ export const appRoutes: Route[] = [
   {
     path: '',
     pathMatch: 'full',
-    loadComponent: () => import('@ng-tool-collection/home').then(m => m.WebsiteComponent),
-    title: 'Tool Collection',
-  },
-  {
-    path: 'tools',
     loadChildren: () => import('@ng-tool-collection/tools').then(m => m.toolsRoutes),
     title: 'Tool Collection',
   },
