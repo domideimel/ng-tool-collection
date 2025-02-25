13db614418f176c021230e904bc7cf501bab6fa7
dominikdeimel <info@dominikdeimel.com>
2025-02-25 20:15:12 +0100

    Refactor routes and navigation structure.
    Replaced home route component with a redirect to "tools" and commented out "Me" navigation link. Adjusted navbar styling to improve dropdown menu alignment. These changes enhance routing clarity and navigation usability.
    
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ export const appRoutes: Route[] = [
   {
     path: '',
     pathMatch: 'full',
+    title: 'Tool Collection',
+    redirectTo: 'tools',
+  },
+  {
+    path: 'tools',
     loadChildren: () => import('@ng-tool-collection/tools').then(m => m.toolsRoutes),
     title: 'Tool Collection',
   },
