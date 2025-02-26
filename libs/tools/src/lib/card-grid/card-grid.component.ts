31bf0e1bb9675ac5965f0ce9e2cc6a538dd76640
dominikdeimel <info@dominikdeimel.com>
2025-02-26 20:29:38 +0100

    Remove unused code and obsolete button from CardGrid
    Eliminated redundant button from the template and unused properties, functions, and imports from the component file. This cleanup improves maintainability and ensures only relevant code remains.
    
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- libs/tools/src/lib/card-grid/card-grid.component.ts
+++ libs/tools/src/lib/card-grid/card-grid.component.ts
@@ -1,14 +1,8 @@
-import { ChangeDetectionStrategy, Component, computed, effect, inject, OnInit, WritableSignal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, OnInit } from '@angular/core';
 import { Link } from '@ng-tool-collection/models';
 import { NAVIGATION } from '@ng-tool-collection/constants';
 import { Meta } from '@angular/platform-browser';
 import { CardComponent } from '@ng-tool-collection/ui';
-import { LocalStorage, SessionStorage } from '@ng-tool-collection/utils';
-
-interface UserData {
-  name: string;
-  age: number;
-}
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -26,20 +20,7 @@ export class CardGridComponent implements OnInit {
     }));
     return children ?? [];
   });
-  @LocalStorage<UserData>('user_data_key')
-  userData = {
-    name: 'John',
-    age: 30,
-  } as unknown as WritableSignal<UserData>;
   private meta = inject(Meta);
-  @SessionStorage('some_key')
-  private test = 'Test' as unknown as WritableSignal<string>;
-
-  constructor() {
-    effect(() => {
-      console.log(this.test());
-    });
-  }
 
   ngOnInit() {
     this.meta.updateTag({
@@ -47,9 +28,5 @@ export class CardGridComponent implements OnInit {
       content:
         'Entdecke unsere umfangreiche Sammlung an Tools, die entwickelt wurden, um deinen Alltag zu erleichtern. Von Produktivitätssteigerung bis hin zur Organisation – finde genau das, was du brauchst, um effizienter zu arbeiten und mehr zu erreichen.',
     });
-  }
-
-  onAddCard() {
-    this.test.set(Math.random().toString());
   }
 }
