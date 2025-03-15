7cbba103ae93ab13cc0c57574246a68023f3e900
dominikdeimel <info@dominikdeimel.com>
2025-03-15 19:22:41 +0100

    Enable view transitions with animations
    Added CSS keyframes and styles for view transitions, including fade and slide effects. Updated Angular router configuration to include the `withViewTransitions` feature.
    
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, isDevMode, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withViewTransitions()),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
