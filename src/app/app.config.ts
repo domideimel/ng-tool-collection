0274477cc3395c323ade95bda80e6928aa3bc0e5
dominikdeimel <info@dominikdeimel.com>
2025-04-02 19:07:55 +0200

    Remove view transition animations and related styles
    Removed custom keyframes and animations for view transitions from `styles.css` and disabled the `withViewTransitions` option in router configuration. This simplifies styling and configuration by eliminating unused or unnecessary animations.
    
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, isDevMode, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(appRoutes, withViewTransitions()),
+    provideRouter(appRoutes),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
