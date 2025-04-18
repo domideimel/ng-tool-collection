cd02e661522caf7bf64eacb735b4a7de8d942556
dominikdeimel <info@dominikdeimel.com>
2025-04-18 15:49:38 +0200

    Refactor UI components to improve styling consistency
    Normalized input, select, and button styles across forms and components for better alignment and uniformity. Applied `input-lg`, `select-lg`, and consistent spacing classes. Additionally, cleaned up redundant HTML classes and simplified the Tailwind configuration.
    
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,40 +1,18 @@
-import { createGlobPatternsForDependencies } from '@nx/angular/tailwind';
+import { createGlobPatternsForDependencies } from "@nx/angular/tailwind";
+import { join } from "path";
 
-import daisyui from 'daisyui';
-import { join } from 'path';
-
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
-  content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'), ...createGlobPatternsForDependencies(__dirname)],
-  daisyui: {
-    themes: [
-      'sunset',
-      'cupcake',
-      {
-        domi: {
-          primary: '#00bbff',
-          secondary: '#00d5f1',
-          accent: '#ff5500',
-          neutral: '#181211',
-          'base-100': '#2e2726',
-          info: '#0082ff',
-          success: '#00c956',
-          warning: '#ff8200',
-          error: '#ff005a',
-        },
-      },
-    ],
-  },
+  content: [join(__dirname, "src/**/!(*.stories|*.spec).{ts,html}"), ...createGlobPatternsForDependencies(__dirname)],
   corePlugins: {
     container: false,
   },
   plugins: [
-    daisyui,
     ({ addComponents, theme }) => {
       addComponents({
-        '.container': {
-          '@apply px-4 mx-auto w-full': {},
-          maxWidth: theme('screens.2xl'),
+        ".container": {
+          "@apply px-4 mx-auto w-full": {},
+          maxWidth: theme("screens.2xl"),
         },
       });
     },
