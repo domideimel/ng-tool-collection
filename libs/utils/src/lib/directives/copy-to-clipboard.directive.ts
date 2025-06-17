8651e22773d9bb9d1b07cd2a0eadbc7679dd3c0f
dominikdeimel <info@dominikdeimel.com>
2025-06-17 21:27:23 +0200

    Add comprehensive tests for clipboard and utility functions
    - Added unit tests for `CopyToClipboardDirective` to validate functionality and error handling.
    - Added tests for `copyToClipboard` utility covering successful clipboard access, error scenarios, and edge cases.
    - Added tests for the lodash utility methods (`range`, `random`, and `sample`).
    - Verified `ClickOutsideDirective` with unit tests ensuring proper event handling.
    
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- libs/utils/src/lib/directives/copy-to-clipboard.directive.ts
+++ libs/utils/src/lib/directives/copy-to-clipboard.directive.ts
@@ -1,17 +1,18 @@
-import { Directive, ElementRef, HostListener, inject, input, OnDestroy, output } from '@angular/core';
-import { State } from '@ng-tool-collection/models';
-import { copyToClipboard } from '../utils/copy-to-clipboard.utils';
-import { catchError, of, Subscription, tap } from 'rxjs';
+import { Directive, ElementRef, HostListener, inject, input, OnDestroy, output } from "@angular/core";
+import { State } from "@ng-tool-collection/models";
+import { copyToClipboard } from "../utils/copy-to-clipboard.utils";
+import { catchError, of, Subscription, tap } from "rxjs";
 
 @Directive({
   // eslint-disable-next-line @angular-eslint/directive-selector
-  selector: '[copyToClipboard]',
+  selector: "[copyToClipboard]",
 })
 export class CopyToClipboardDirective<T> implements OnDestroy {
   copyToClipboard = input<T>();
 
   copyError = output<State.ERROR>();
   success = output<State.SUCCESS>();
+  copied = output<State.SUCCESS | State.ERROR>();
 
   private subscription: Subscription | undefined;
   private elementRef: ElementRef<T> = inject(ElementRef);
@@ -20,8 +21,8 @@ export class CopyToClipboardDirective<T> implements OnDestroy {
     this.subscription?.unsubscribe();
   }
 
-  @HostListener('click')
-  @HostListener('touchstart')
+  @HostListener("click")
+  @HostListener("touchstart")
   copy() {
     const textToCopy = this.copyToClipboard() ?? (this.elementRef.nativeElement as HTMLElement).innerText;
 
@@ -32,8 +33,10 @@ export class CopyToClipboardDirective<T> implements OnDestroy {
     this.subscription = copyToClipboard(textToCopy)
       .pipe(
         tap(() => this.success.emit(State.SUCCESS)),
+        tap(() => this.copied.emit(State.SUCCESS)),
         catchError(() => {
           this.copyError.emit(State.ERROR);
+          this.copied.emit(State.ERROR);
           return of(null);
         }),
       )
