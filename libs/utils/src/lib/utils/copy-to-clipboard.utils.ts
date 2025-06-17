e1917c15e5ae82c66c8df1996e4a003d9cfbcca1
dominikdeimel <info@dominikdeimel.com>
2025-06-17 21:08:33 +0200

    Replace `fromPromise` with `from` in copy-to-clipboard utility.
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- libs/utils/src/lib/utils/copy-to-clipboard.utils.ts
+++ libs/utils/src/lib/utils/copy-to-clipboard.utils.ts
@@ -1,6 +1,5 @@
-import { catchError, map, Observable, of, switchMap, throwError } from "rxjs";
+import { catchError, from, map, Observable, of, switchMap, throwError } from "rxjs";
 import { State } from "@ng-tool-collection/models";
-import { fromPromise } from "rxjs/internal/observable/innerFrom";
 
 export const copyToClipboard = <T>(data: T) => {
   if (!navigator?.clipboard || !navigator?.clipboard?.writeText || !data) {
@@ -10,7 +9,7 @@ export const copyToClipboard = <T>(data: T) => {
   const dataToCopy$ = of(data).pipe(map(data => (typeof data === "string" ? data : JSON.stringify(data))));
 
   return dataToCopy$.pipe(
-    switchMap(dataToCopy => fromPromise(navigator.clipboard.writeText(dataToCopy))),
+    switchMap(dataToCopy => from(navigator.clipboard.writeText(dataToCopy))),
     catchError(err => {
       console.error(err);
       return throwError(() => State.ERROR) as Observable<State>;
