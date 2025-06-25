2b277dd2a3c6edc6e632ffb9ab491a8b34306532
dominikdeimel <info@dominikdeimel.com>
2025-06-25 19:45:22 +0200

    Refactor ToastService to replace numeric IDs with UUIDs and standardize string formatting in toast models.
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- libs/ui/src/lib/components/toasts/services/toasts.service.ts
+++ libs/ui/src/lib/components/toasts/services/toasts.service.ts
@@ -1,33 +1,32 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { ToastMessage, ToastType } from '@ng-tool-collection/models';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { ToastMessage, ToastType } from "@ng-tool-collection/models";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class ToastService {
   private toastMessagesSubject = new BehaviorSubject<ToastMessage[]>([]);
   toasts = this.toastMessagesSubject.asObservable();
-  private nextId = 0;
 
   success(message: string) {
-    this.showToast(message, 'success');
+    this.showToast(message, "success");
   }
 
   error(message: string) {
-    this.showToast(message, 'error');
+    this.showToast(message, "error");
   }
 
   info(message: string) {
-    this.showToast(message, 'info');
+    this.showToast(message, "info");
   }
 
   warning(message: string) {
-    this.showToast(message, 'warning');
+    this.showToast(message, "warning");
   }
 
   private showToast(message: string, type: ToastType) {
-    const toast: ToastMessage = { id: this.nextId++, message, type };
+    const toast: ToastMessage = { id: crypto.randomUUID(), message, type };
 
     // Add the new toast message to the array
     this.toastMessagesSubject.next([...this.toastMessagesSubject.value, toast]);
@@ -35,7 +34,7 @@ export class ToastService {
     setTimeout(() => this.removeToast(toast.id), 3000);
   }
 
-  private removeToast(id: number) {
+  private removeToast(id: string) {
     const updatedMessages = this.toastMessagesSubject.value.filter(toast => toast.id !== id);
     this.toastMessagesSubject.next(updatedMessages);
   }
