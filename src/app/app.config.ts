f8691a49693166195bedcb40921bb13a898dc794
dominikdeimel <info@dominikdeimel.com>
2025-06-25 20:51:11 +0200

    Enforce single quotes in Prettier and standardize code style in configuration files.
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,15 @@
-import { ApplicationConfig, isDevMode, provideZonelessChangeDetection } from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { appRoutes } from "./app.routes";
-import { provideServiceWorker } from "@angular/service-worker";
-import { provideHttpClient, withFetch } from "@angular/common/http";
+import { ApplicationConfig, isDevMode, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
-    provideServiceWorker("ngsw-worker.js", {
+    provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
-      registrationStrategy: "registerWhenStable:30000",
+      registrationStrategy: 'registerWhenStable:30000',
     }),
     provideZonelessChangeDetection(),
     provideHttpClient(withFetch()),
