3ea2af36aa01b54c7767fdf199f901f856612341
dominikdeimel <info@dominikdeimel.com>
2025-07-03 17:43:31 +0200

    Apply Prettier formatting to `LoadingService` for consistent code style.
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- libs/utils/src/lib/services/loading.service.ts
+++ libs/utils/src/lib/services/loading.service.ts
@@ -33,9 +33,7 @@ export class LoadingService {
     observables: ObservableMap;
     stateChangeDelay?: number;
     deleteLoadingState?: boolean;
-  }): Observable<{
-    [K in keyof ObservableMap]: ObservableMap[K] extends Observable<infer T> ? T : never;
-  }> {
+  }) {
     const identifiers = Array.isArray(identifier) ? identifier : [identifier];
 
     return concat(
