e90f97f9abccceeb7376316c69eb6634bad8af2f
dominikdeimel <info@dominikdeimel.com>
2025-07-14 20:10:40 +0200

    Apply consistent formatting to HTML and TypeScript files to align with coding standards.
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- libs/password-generator/src/lib/components/generator-form/generator-form.component.ts
+++ libs/password-generator/src/lib/components/generator-form/generator-form.component.ts
@@ -1,57 +1,56 @@
-import { ChangeDetectionStrategy, Component, inject, OnDestroy, signal } from "@angular/core";
-import { FormModel, GenerationProperties } from "@ng-tool-collection/models";
-import { Validators } from "@angular/forms";
-import { PasswordGeneratorService } from "../../services/password-generator.service";
-import { atLeastOneCheckedValidator, CardComponent, FormComponent, ToastService } from "@ng-tool-collection/ui";
-import { catchError, finalize, Subscription, tap } from "rxjs";
-import { copyToClipboard, ReactiveStorageService } from "@ng-tool-collection/utils";
-import { NgpButton } from "ng-primitives/button";
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, signal } from '@angular/core';
+import { FormModel, GenerationProperties } from '@ng-tool-collection/models';
+import { Validators } from '@angular/forms';
+import { PasswordGeneratorService } from '../../services/password-generator.service';
+import { atLeastOneCheckedValidator, CardComponent, FormComponent, ToastService } from '@ng-tool-collection/ui';
+import { catchError, finalize, Subscription, tap } from 'rxjs';
+import { copyToClipboard, ReactiveStorageService } from '@ng-tool-collection/utils';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  selector: "lib-generator-form",
-  templateUrl: "./generator-form.component.html",
-  imports: [CardComponent, FormComponent, NgpButton],
+  selector: 'lib-generator-form',
+  templateUrl: './generator-form.component.html',
+  imports: [CardComponent, FormComponent],
 })
 export class GeneratorFormComponent implements OnDestroy {
   formModel = {
     items: [
       {
-        label: "Passwort Länge",
-        controlName: "length",
-        type: "range",
+        label: 'Passwort Länge',
+        controlName: 'length',
+        type: 'range',
         value: 10,
         validators: [Validators.required, Validators.min(6), Validators.max(100)],
       },
       {
-        label: "Großbuchstaben verwenden",
-        controlName: "upper",
-        type: "checkbox",
+        label: 'Großbuchstaben verwenden',
+        controlName: 'upper',
+        type: 'checkbox',
         value: true,
       },
       {
-        label: "Kleinbuchstaben verwenden",
-        controlName: "lower",
-        type: "checkbox",
+        label: 'Kleinbuchstaben verwenden',
+        controlName: 'lower',
+        type: 'checkbox',
         value: true,
       },
       {
-        label: "Sonderzeichen verwenden",
-        controlName: "symbol",
-        type: "checkbox",
+        label: 'Sonderzeichen verwenden',
+        controlName: 'symbol',
+        type: 'checkbox',
         value: true,
       },
       {
-        label: "Zahlen verwenden",
-        controlName: "number",
-        type: "checkbox",
+        label: 'Zahlen verwenden',
+        controlName: 'number',
+        type: 'checkbox',
         value: true,
       },
     ],
-    submitButtonLabel: "Passwort generieren",
-    customValidators: atLeastOneCheckedValidator(["upper", "lower", "symbol", "number"]),
+    submitButtonLabel: 'Passwort generieren',
+    customValidators: atLeastOneCheckedValidator(['upper', 'lower', 'symbol', 'number']),
   } as const satisfies FormModel;
-  password = signal<string>("");
+  password = signal<string>('');
   hasCopied = signal<boolean>(true);
   private subscription = new Subscription();
   private passwordGeneratorService = inject(PasswordGeneratorService);
@@ -77,14 +76,14 @@ export class GeneratorFormComponent implements OnDestroy {
     const copySub = copyToClipboard(this.password())
       .pipe(
         tap(() => {
-          const passwords = this.storageService.getItem<string[]>("passwords");
+          const passwords = this.storageService.getItem<string[]>('passwords');
           const oldPasswords = passwords() ? passwords() : [];
-          this.storageService.setItem("passwords", [this.password(), ...oldPasswords]);
+          this.storageService.setItem('passwords', [this.password(), ...oldPasswords]);
           this.hasCopied.set(true);
-          this.toast.success("Passwort wurde erfolgreich kopiert");
+          this.toast.success('Passwort wurde erfolgreich kopiert');
         }),
         catchError(err => {
-          this.toast.error("Beim kopieren ist etwas schief gelaufen");
+          this.toast.error('Beim kopieren ist etwas schief gelaufen');
           return err;
         }),
       )
