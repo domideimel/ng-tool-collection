7852d97bcb106cfd88606deb88f00c7c6929bf3b
dominikdeimel <info@dominikdeimel.com>
2025-07-25 20:44:47 +0200

    Refactor: Replace custom `Toasts` and `ThemeSwitcher` components with PrimeNG's `Toast` and `ConfirmDialog`. Remove corresponding services and update associated UI components and templates.
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,53 @@
-import { ChangeDetectionStrategy, Component, computed, effect, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent, ServiceWorkerUpdatePromptComponent, ToastsComponent } from '@ng-tool-collection/ui';
+import { NavbarComponent, ServiceWorkerUpdatePromptComponent } from '@ng-tool-collection/ui';
 import { Button } from 'primeng/button';
-import { Dialog } from 'primeng/dialog';
+import { Toast } from 'primeng/toast';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialog } from 'primeng/confirmdialog';
 
 @Component({
-  imports: [RouterModule, ToastsComponent, ServiceWorkerUpdatePromptComponent, Button, Dialog, NavbarComponent],
+  imports: [ConfirmDialog, RouterModule, ServiceWorkerUpdatePromptComponent, Button, NavbarComponent, Toast],
   selector: 'app-root',
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [ConfirmationService, MessageService],
 })
 export class AppComponent {
-  visibleAnalytics = signal(false);
   currentYear = computed(() => {
     return new Date().getFullYear();
   });
+
+  private confirmationService = inject(ConfirmationService);
+  private messageService = inject(MessageService);
 
   constructor() {
     const analytics = localStorage.getItem('va-disable');
     effect(() => {
       if (!analytics) {
-        this.openModal();
+        this.confirm();
       }
     });
   }
 
-  openModal() {
-    this.visibleAnalytics.set(true);
+  confirm() {
+    this.confirmationService.confirm({
+      header: 'Vercel Analytic',
+      message: 'Diese Seite verwendet Vercel Analytics',
+      accept: () => {
+        this.messageService.add({ severity: 'info', summary: 'Akzeptiert', detail: 'Du hast akzeptiert' });
+        this.accept('false');
+      },
+      reject: () => {
+        this.messageService.add({ severity: 'info', summary: 'Abgelehnt', detail: 'Du hast abgelehnt' });
+        this.accept('true');
+      },
+    });
   }
 
-  closeModal(accept: 'true' | 'false') {
+  private accept(accept: 'true' | 'false') {
     const formerValue = localStorage.getItem('va-disable');
     localStorage.setItem('va-disable', accept);
-
-    this.visibleAnalytics.set(false);
 
     if (accept !== formerValue) {
       window.location.reload();
