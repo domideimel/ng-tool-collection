638a4d3d1944d220a596d2a28fe7aabc37c924fb
dominikdeimel <info@dominikdeimel.com>
2025-07-25 20:20:05 +0200

    Refactor `CardComponent` to use PrimeNG `Card` and `Button`. Update bindings, methods, and template structure. Adjust `NavbarComponent` styles for consistency.
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- libs/ui/src/lib/components/card/card.component.ts
+++ libs/ui/src/lib/components/card/card.component.ts
@@ -1,16 +1,26 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Card } from 'primeng/card';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'lib-card',
   templateUrl: './card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterLink],
+  imports: [Card, Button],
 })
 export class CardComponent {
-  title = input.required<string>();
+  cardTitle = input.required<string>({
+    alias: 'title',
+  });
   description = input<string>('');
   link = input<string>('');
   linkText = input<string>('Zum Tool');
   showLink = input<boolean>(true);
+
+  private router = inject(Router);
+
+  onLinkClick() {
+    this.router.navigate([this.link()]);
+  }
 }
