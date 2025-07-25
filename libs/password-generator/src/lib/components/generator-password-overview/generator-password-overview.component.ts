aff4ef9c46c74d3a756b30debeeaeb726c7c5477
dominikdeimel <info@dominikdeimel.com>
2025-07-25 21:41:46 +0200

    Refactor `GeneratorPasswordOverviewComponent` to use PrimeNG `Button`. Update template, styles, and dependencies to replace custom buttons with PrimeNG components.
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- libs/password-generator/src/lib/components/generator-password-overview/generator-password-overview.component.ts
+++ libs/password-generator/src/lib/components/generator-password-overview/generator-password-overview.component.ts
@@ -2,12 +2,13 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CardComponent } from '@ng-tool-collection/ui';
 import { CopyToClipboardDirective, ReactiveStorageService } from '@ng-tool-collection/utils';
 import { MessageService } from 'primeng/api';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'lib-generator-password-overview',
   templateUrl: './generator-password-overview.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CardComponent, CopyToClipboardDirective],
+  imports: [CardComponent, CopyToClipboardDirective, Button],
 })
 export class GeneratorPasswordOverviewComponent {
   private storageService = inject(ReactiveStorageService);
