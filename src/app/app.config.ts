b53a30743f201837cdb826f340780de8473b8469
dominikdeimel <info@dominikdeimel.com>
2025-12-17 18:52:43 +0100

    Remove unused `colorScheme` configuration from `app.config.ts`.
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -30,40 +30,6 @@ const MyPreset = definePreset(Material, {
       900: '{rose.900}',
       950: '{rose.950}',
     },
-    colorScheme: {
-      light: {
-        surface: {
-          0: '{slate.950}',
-          50: '{slate.900}',
-          100: '{slate.800}',
-          200: '{slate.700}',
-          300: '{slate.600}',
-          400: '{slate.500}',
-          500: '{slate.400}',
-          600: '{slate.300}',
-          700: '{slate.200}',
-          800: '{slate.100}',
-          900: '{slate.50}',
-          950: '{slate.50}',
-        },
-      },
-      dark: {
-        surface: {
-          0: '#ffffff',
-          50: '{slate.50}',
-          100: '{slate.100}',
-          200: '{slate.200}',
-          300: '{slate.300}',
-          400: '{slate.400}',
-          500: '{slate.500}',
-          600: '{slate.600}',
-          700: '{slate.700}',
-          800: '{slate.800}',
-          900: '{slate.900}',
-          950: '{slate.950}',
-        },
-      },
-    },
   },
 });
 
