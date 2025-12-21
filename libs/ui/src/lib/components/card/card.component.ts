41de45bc93beab5f17edd8ad6709f904324e9132
dominikdeimel <info@dominikdeimel.com>
2025-12-21 18:03:15 +0100

    Enhance `CardComponent` with `fullHeight` input and refine layout; update `CardGridComponent` to utilize new `fullHeight` feature for improved design flexibility.
    Signed-off-by: dominikdeimel <info@dominikdeimel.com>

--- libs/ui/src/lib/components/card/card.component.ts
+++ libs/ui/src/lib/components/card/card.component.ts
@@ -2,12 +2,13 @@ import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core
 import { Router } from '@angular/router';
 import { Card } from 'primeng/card';
 import { Button } from 'primeng/button';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'lib-card',
   templateUrl: './card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [Card, Button],
+  imports: [Card, Button, NgClass],
 })
 export class CardComponent {
   cardTitle = input.required<string>({
@@ -17,6 +18,7 @@ export class CardComponent {
   link = input<string>('');
   linkText = input<string>('Zum Tool');
   showLink = input<boolean>(true);
+  fullHeight = input<boolean>(false);
 
   private router = inject(Router);
 
